b331964ed50c8b656725b6036dc0484fcd7bbf1a
Ruslan <88377783+Dealer327@users.noreply.github.com>
2023-02-08 22:25:52 +0300

    Initial commit

--- diplom_project/main/urls.py
+++ diplom_project/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('help', views.help_me, name='help')
+    path('help', views.help_me, name='help'),
+    path('sing_up', views.sing_up, name='sing_up')
 ]
--- diplom_project/main/views.py
+++ diplom_project/main/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index/main.html')
+    return render(request, 'main/index/base.html')
 
 
 def help_me(request):
     return render(request, 'main/index/help.html')
+
+
+def sing_up(request):
+    return render(request, 'main/index/sing_up.html')
