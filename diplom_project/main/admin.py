7e8f282daa5100730c9404f4f7f6d8a51d53d075
Ruslan <88377783+Dealer327@users.noreply.github.com>
2023-02-21 17:24:22 +0300

    работа с Админ панелью

--- diplom_project/main/admin.py
+++ diplom_project/main/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 
 # Register your models here.
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('Login', 'Password', 'Email')
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'time_creat', 'photo', 'is_published')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+admin.site.register(Posts, PostsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- diplom_project/main/models.py
+++ diplom_project/main/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Posts(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
     content = models.TextField(max_length=500)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d")
     time_creat = models.DateTimeField(auto_now_add=True)
@@ -20,6 +20,9 @@ class Posts(models.Model):
     def get_absolute_url(self):
         """Формирования динамических ссылок на посты"""
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        ordering = ['-time_creat', 'title']
 
 
 class Category(models.Model):
